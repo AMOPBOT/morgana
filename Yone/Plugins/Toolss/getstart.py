6e27a709083bcb0d86b3d6193a4e0de37ff00240
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-22 12:08:30 +0530

    Update getstart.py

--- Yone/Plugins/Toolss/getstart.py
+++ Yone/Plugins/Toolss/getstart.py
@@ -5,7 +5,8 @@ from pyrogram.enums import ChatAction, ParseMode
 from Yone import OWNER_ID, DEEP_API, pbot as app
 
 
-__help__ = """ɢᴇᴛ sᴛᴀʀᴛᴇᴅ ᴡɪᴛʜ ʙᴏᴛ
+__help__ = """ɢᴇᴛ sᴛᴀʀᴛᴇᴅ ᴡɪᴛʜ ʙᴏᴛ :
+
 /start : sᴛᴀʀᴛs ᴛʜᴇ ᴍᴜsɪᴄ ʙᴏᴛ.
 /help : ɢᴇᴛ ʜᴇʟᴩ ᴍᴇɴᴜ ᴡɪᴛʜ ᴇxᴩʟᴀɴᴀᴛɪᴏɴ ᴏғ ᴄᴏᴍᴍᴀɴᴅs.
 /reboot : ʀᴇʙᴏᴏᴛs ᴛʜᴇ ʙᴏᴛ ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ.
