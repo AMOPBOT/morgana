111538c2e1e7f38e043757e1024e7ca9f9849f2f
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-22 12:07:48 +0530

    Update authuser.py

--- Yone/Plugins/Toolss/authuser.py
+++ Yone/Plugins/Toolss/authuser.py
@@ -5,8 +5,11 @@ from pyrogram.enums import ChatAction, ParseMode
 from Yone import OWNER_ID, DEEP_API, pbot as app
 
 
-__help__ = """ᴀᴜᴛʜ ᴜsᴇʀs  ᴄᴏᴍᴍᴀɴᴅs
+__help__ = """
 ᴀᴜᴛʜ ᴜsᴇʀs ᴄᴀɴ ᴜsᴇ ᴀᴅᴍɪɴ ʀɪɢʜᴛs ɪɴ ᴛʜᴇ ʙᴏᴛ ᴡɪᴛʜᴏᴜᴛ ᴀᴅᴍɪɴ ʀɪɢʜᴛs ɪɴ ᴛʜᴇ ᴄʜᴀᴛ. [ᴀᴅᴍɪɴs ᴏɴʟʏ]
+
+ᴀᴜᴛʜ ᴜsᴇʀs ᴄᴏᴍᴍᴀɴᴅs :
+
 /auth [ᴜsᴇʀɴᴀᴍᴇ] : ᴀᴅᴅ ᴀ ᴜsᴇʀ ᴛᴏ ᴀᴜᴛʜ ʟɪsᴛ ᴏғ ᴛʜᴇ ʙᴏᴛ.
 /unauth [ᴜsᴇʀɴᴀᴍᴇ] : ʀᴇᴍᴏᴠᴇ ᴀ ᴀᴜᴛʜ ᴜsᴇʀs ғʀᴏᴍ ᴛʜᴇ ᴀᴜᴛʜ ᴜsᴇʀs ʟɪsᴛ.
 /authusers : sʜᴏᴡs ᴛʜᴇ ᴀᴜᴛʜ ᴜsᴇʀs ʟɪsᴛ ᴏғ ᴛʜᴇ ɢʀᴏᴜᴩ.
