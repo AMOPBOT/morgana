dadeefe3b30a67c3952f4c6844f7f67527cda85a
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2024-01-03 21:45:11 +0530

    Update __init__.py

--- Yone/__init__.py
+++ Yone/__init__.py
@@ -56,7 +56,7 @@ if ENV:
     except ValueError:
         raise Exception("Please Add Hash Api key to start the bot")
 
-    DB_URI = os.environ.get("DATABASE_URL","postgres://cbtysxae:AxhFIfnAH0KCMVtOwAUE25shztdqb2P9@peanut.db.elephantsql.com/cbtysxae")
+    DB_URI = os.environ.get("DATABASE_URL","postgres://mqcodhup:WHSi5X3wFJwDQeTl8PXS4hb-PInzGxQo@bubble.db.elephantsql.com/mqcodhup")
     PHOTO = os.environ.get("PHOTO", "https://graph.org/file/2d3f35226a0d59cbb9980.jpg") # Miss Poppy Pic
     WORKERS = int(os.environ.get("WORKERS", 8))
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
